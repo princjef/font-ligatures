d6c4c536ce963220b416e3c16a07a00957fe3e45
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2020-06-20 20:34:43 +0530

    fix: check if font.tables.gsub is undefined

--- src/index.ts
+++ src/index.ts
@@ -30,13 +30,12 @@ class FontImpl implements Font {
             });
         }
 
-        const caltFeatures = this._font.tables.gsub.features.filter(f => f.tag === 'calt');
+        const caltFeatures = this._font.tables.gsub && this._font.tables.gsub.features.filter(f => f.tag === 'calt') || [];
         const lookupIndices: number[] = caltFeatures
             .reduce((acc, val) => [...acc, ...val.feature.lookupListIndexes], []);
-        const lookupGroups = this._font.tables.gsub.lookups
-            .filter((l, i) => lookupIndices.some(idx => idx === i));
 
-        const allLookups = this._font.tables.gsub.lookups;
+        const allLookups = this._font.tables.gsub && this._font.tables.gsub.lookups || [];
+        const lookupGroups = allLookups.filter((l, i) => lookupIndices.some(idx => idx === i));
 
         for (const [index, lookup] of lookupGroups.entries()) {
             const trees: LookupTree[] = [];
